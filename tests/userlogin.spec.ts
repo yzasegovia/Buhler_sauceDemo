44a1a04f9540d90e5c46f260f642d7fe2ba22b37
Yza Segovia <yza.anne.v.segovia@gmail.com>
2024-04-30 20:03:32 +0200

    added invalid user login test and code formatting

--- tests/userlogin.spec.ts
+++ tests/userlogin.spec.ts
@@ -2,41 +2,38 @@ import { test, expect } from '@playwright/test';
 
  // Defining constants
 const USER_NAME_INPUT = '[data-test="username"]';
-const USER_PASSWORD = '[data-test="password"]';
+const USER_PWD_INPUT = '[data-test="password"]';
 const LOGIN_ERROR = '[data-test="error"]';
-const LOGIN_BUTTON = '[data-test="login-button"]';
+const USER_LOGIN_BUTTON = '[data-test="login-button"]';
+const USER_LOGOUT_BUTTON = '[data-test="logout-sidebar-link"]';
 
-test.describe('login', () =>{
+test.describe('User login operation', () =>{
     test.beforeEach(async({ page }) => {
         await page.goto('https://www.saucedemo.com/');
     });
 
     // To test standard_user login
-    test('validLogin', async ({ page }) => {
+    test('Testing standard user valid login', async ({ page }) => {
         //await page.locator(USER_NAME_INPUT ).click();
         await page.locator(USER_NAME_INPUT ).fill('standard_user');
         //await page.locator(USER_NAME_INPUT ).press('Tab');
-        await page.locator(USER_PASSWORD).fill('secret_sauce');
-        await page.locator(LOGIN_BUTTON).click();
+        await page.locator(USER_PWD_INPUT).fill('secret_sauce');
+        await page.locator(USER_LOGIN_BUTTON).click();
 
-    // Check if login was successful
-    const loggedInUrl = page.url();
-    if (loggedInUrl === 'https://www.saucedemo.com/inventory.html') {
-        console.log('Login test passed! User successfully logged in.');
-    } else {
-        console.error('Login test failed! User not logged in.');
-    }
+        // Check if login was successful
+        await expect(page.locator(USER_LOGOUT_BUTTON)).not.toBeNull();
+    
     });
 
     // To test locked_out_user credential
-    test('lockedLogin', async ({ page }) => {
+    test('Testing invalid login using locked_user credential', async ({ page }) => {
         //await page.locator(USER_NAME_INPUT ).click();
         await page.locator(USER_NAME_INPUT ).fill('locked_out_user');
         //await page.locator(USER_NAME_INPUT ).press('Tab');
-        await page.locator(USER_PASSWORD).fill('secret_sauce');
-        await page.locator(LOGIN_BUTTON).click();
-    
+        await page.locator(USER_PWD_INPUT).fill('secret_sauce');
+        await page.locator(USER_LOGIN_BUTTON).click();
+
         // Check if login error is shown
         await expect(page.locator(LOGIN_ERROR)).toBeVisible();
-        });
+    });
 });
